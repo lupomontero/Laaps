d767d113db55483638cdef961b9d11a88a73f978
Oralia <oraliagarrido@gmail.com>
2020-08-25 10:58:10 -0500

    add db to users

--- src/components/admin /index.js
+++ src/components/admin /index.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { db, functions } from "../firebase.js";
 
 const Admin = () => {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      await db.collection("users");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <h1>A qui se vera el entorno de la administracion y empleados</h1>;
 };
 
